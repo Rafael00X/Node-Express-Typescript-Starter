a533b8198b9651f98a3c592b248f6f3543948367
Subhadeep <amsubhadeep@gmail.com>
2023-09-21 09:57:34 +0530

    Initial setup

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = 8000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello TS Node Dev</h1>");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`);
+});
